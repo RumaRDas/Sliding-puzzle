86a046ae8d8d192ab7213b3f543a819ffbe49f79
Ruma Das <rumaadl16@gmail.com>
2022-05-13 21:34:31 +0930

    adding responsive navbar

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,38 +1,58 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/logo.png'
+import { useMediaQuery } from 'react-responsive'
 import './style.css'
 
 const Header = () => {
+  const smallDevice = useMediaQuery({
+    query: '(max-width: 800px)',
+  })
+
   return (
-    <nav id="header" className="navbar-Items fixed-top">
+    <nav className="navbar-Items fixed-top" id="header">
       <div className="navbar-logo">
         <img src={Logo} alt="logo" className="logo-img" />
       </div>
       <ul className="nav-menu">
         <li className="nav-list">
-          <Link className="nav-links" to="/">
-            Home
+          <Link to="/" className="nav-links">
+            {smallDevice ? (
+              <dt className="nav-style">H</dt>
+            ) : (
+              <dt className="nav-style">Home</dt>
+            )}
           </Link>
         </li>
         <li className="nav-list">
-          <Link className="nav-links" to="/blanko">
-            Blanko
+          <Link to="/blanko" className="nav-links">
+            {smallDevice ? (
+              <dt className="nav-style">B</dt>
+            ) : (
+              <dt className="nav-style">Blanko</dt>
+            )}
           </Link>
         </li>
         <li className="nav-list">
-          <Link className="nav-links" to="/slido">
-            Slido
+          <Link to="/slido" className="nav-links">
+            {smallDevice ? (
+              <dt className="nav-style">S</dt>
+            ) : (
+              <dt className="nav-style">Slido</dt>
+            )}
           </Link>
         </li>
         <li className="nav-list">
-          <Link className="nav-links border-style" to="/tetro">
-            Tetro
+          <Link to="/tetro" className="nav-links">
+            {smallDevice ? (
+              <dt className="nav-style border-style">T</dt>
+            ) : (
+              <dt className="nav-style border-style">Tetro</dt>
+            )}
           </Link>
         </li>
       </ul>
     </nav>
   )
 }
-
 export default Header
