0920ee1020d19d9cd125d5a9b7f82f3eb39e0df8
Ruma Das <rumaadl16@gmail.com>
2022-05-13 23:52:31 +0930

    adding input field

--- src/pages/blanko/Blanko.js
+++ src/pages/blanko/Blanko.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { strs } from '../../data/blanko'
+import './style.css'
 
 const Blanko = () => {
+  const word = [...strs]
+  console.log(word)
+
   return (
-    <div>Blanko</div>
+    <form>
+      <div className="row blanko-H-style">
+        <h3>Blanko</h3>
+      </div>
+      <div className="row blanko-stye">
+        <input type="text" className="box-style " maxLength={1}></input>
+        <div className="btn-div">
+          <button className="button-style">Reset</button>
+        </div>
+      </div>
+    </form>
   )
 }
 
