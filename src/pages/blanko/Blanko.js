c1dcf9eeb6b50fc05e7ff67ba7f7b98135456136
Ruma Das <rumaadl16@gmail.com>
2022-05-16 22:01:45 +0930

    changing some css and getting data in inside Box

--- src/pages/blanko/Blanko.js
+++ src/pages/blanko/Blanko.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { strs } from '../../data/blanko'
 import './style.css'
 
@@ -7,21 +7,41 @@ const Blanko = () => {
   console.log(word)
   //getting Random word
   const singleWord = word[Math.floor(Math.random() * word.length)]
-  console.log('Getting Single Word : ', singleWord)
+  // console.log('Getting Single Word : ', singleWord)
 
   //  getting each character from ramdom word
-  const eahCharacter = Array.from(singleWord)
-  console.log('Character: ', eahCharacter)
+  const singleCharacter = Array.from(singleWord)
+  // console.log('Character: ', eahCharacter)
+  //slicing the character
+  const matchCharacters = singleCharacter.slice(3)
+  // console.log('matchCharacter :', matchCharacters)
+  //declearing state
+  const [values, setValues] = useState(singleCharacter)
+
+  const selectInput = values.map((value, index) => {
+    return (
+      <input
+        type="text"
+        className="box-style "
+        maxLength={1}
+        name={index}
+        value={value.index}
+        placeholder={value}
+      ></input>
+    )
+  })
 
   return (
-    <form>
+    <form className="blanko_form">
       <div className="row blanko-H-style">
         <h3>Blanko</h3>
       </div>
       <div className="row blanko-stye">
-        <input type="text" className="box-style " maxLength={1}></input>
-        <div className="btn-div">
-          <button className="button-style">Reset</button>
+        {selectInput}
+        <div className="row btn-div">
+          <button className="button-style">Submit</button>
+
+          <button className="button1-style">Reset</button>
         </div>
       </div>
     </form>
